5b2124589b5205d2a034ba40d421de53d84bf117
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-06-06 07:33:05 -0700

    Finished this file

--- src/net/angusbeefgaming/status/StatusMain.java
+++ src/net/angusbeefgaming/status/StatusMain.java
@@ -2,5 +2,12 @@ package net.angusbeefgaming.status;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StatusMain extends JavaPlugin {
-  
+  @Override
+  public void onEnable() {
+    
+  }
+  @Override
+  public void onDisable() {
+    
+  }
 }
