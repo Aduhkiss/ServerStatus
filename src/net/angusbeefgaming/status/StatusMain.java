30072b27fb62f03cf225e9ea7025d60595c130e2
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-06-15 17:11:34 -0700

    Added this little message

--- src/net/angusbeefgaming/status/StatusMain.java
+++ src/net/angusbeefgaming/status/StatusMain.java
@@ -1,6 +1,14 @@
 package net.angusbeefgaming.status;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+
+Status Plugin
+by Atticus Zambrana (TheBeefSteak)
+
+Get the current Server Status
+
+*/
 public class StatusMain extends JavaPlugin {
   @Override
   public void onEnable() {
